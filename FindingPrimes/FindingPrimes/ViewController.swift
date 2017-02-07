44782e3fa5cf98bfca07f74bed47fc8f3f21e800
BrianRod <brod4910@gmail.com>
2017-02-06 18:48:56 -0700

    Basic application for finding prime numbers

---
+++ FindingPrimes/FindingPrimes/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  FindingPrimes
+//
+//  Created by Brian Rodriguez on 2/6/17.
+//  Copyright © 2017 Brian Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet var result: UILabel!
+	
+	@IBOutlet var userNumber: UITextField!
+	
+	@IBAction func findPrime(_ sender: Any)
+	{
+		var isPrime = true;
+		
+		let userNum = Int(userNumber.text!)!
+		
+		if userNum > 1
+		{
+			for i in (2..<userNum)
+			{
+				if userNum % i == 0
+				{
+					isPrime = false;
+				}
+			}
+			
+			if isPrime == true
+			{
+				result.text = "This is a prime number!";
+			}
+			else
+			{
+				result.text = "This is not a prime number.";
+			}
+		}
+		else
+		{
+			result.text = "Enter a number greater than 1.";
+		}
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
